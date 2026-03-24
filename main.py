e079166d2b4c3be1fdedf1a44b35a6e0b5f43116
joelbooris26-del <joelbooris26@gmail.com>
2026-03-24 02:06:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from datetime import datetime
 from typing import Optional
 
 # ---------------- CONFIG ----------------
-TOKEN = os.environ.get("MTQ4NTYyMTY3NDY2MDMzMTYzMA.GlnIaN.X3Z4fx1XRx1bUDc4qQPJ5dZMG5IPXlxDC516oc")
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
 OWNER_ID = 583251995729723393
 LOG_CHANNEL_ID = 1485699759736885318
